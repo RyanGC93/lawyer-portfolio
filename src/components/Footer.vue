8da23ec35219f232aa72b46b8d09fbfb99793691
Ryan Conk <ryangconk@gmail.com>
2024-07-27 16:09:25 -0600

    add a footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer class="footer">
+    <p>© 2024 Lawyer Landing Page. All rights reserved.</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
+
+<style scoped>
+.footer {
+  background-color: #333;
+  color: #fff;
+  padding: 20px 0;
+  text-align: center;
+}
+</style>
